fab82509399004a91ae3477c912d72943d30e51c
Marc <leidlmairM21047@sus.htl-grieskirchen.at>
2023-10-13 08:41:53 +0200

    change

---
+++ src/Streams.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+public class Streams {
+    public double average(int[] numbers) {
+        //implement this
+    }
+
+    public List<String> upperCase(String[] strings) {
+        //implement this
+    }
+
+    public Weapons findWeaponWithLowestDamage(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public Weapons findWeaponWithHighestStrength(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public List<Weapons> collectMissileWeapons(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public Weapons findWeaponWithLongestName(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public List<String> toNameList(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public int[] toSpeedArray(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public int sumUpValues(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public long sumUpHashCodes(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public List<Weapons> removeDuplicates(List<Weapons> weapons) {
+        //implement this
+    }
+
+    public void increaseValuesByTenPercent(List<Weapons> weapons) {
+        //implement this
+    }
+}
